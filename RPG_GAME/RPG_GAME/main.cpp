e203562d69d5baa422caeb914b12e3aba4710ae5
GraphicsCodeBase <107340913+GraphicsCodeBase@users.noreply.github.com>
2024-04-20 09:57:52 +0800

    adding SDL libs and creating first window.
    created the first window and set the background color to green.

---
+++ RPG_GAME/RPG_GAME/main.cpp
@@ -0,0 +1,23 @@
+#include "SDL.h"
+
+int main(int argc,char *argv[])
+{
+	//init SDL ?
+	SDL_Init(SDL_INIT_EVERYTHING);
+	//creating an SDL window.
+	SDL_Window* window = SDL_CreateWindow("first RPG", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer* Renderer = SDL_CreateRenderer(window, -1, 0);
+
+	//setting SDL drawColor
+	//setting the background color to green.
+	SDL_SetRenderDrawColor(Renderer, 0, 255, 0, 255);
+
+	//clear the screen.
+	SDL_RenderClear(Renderer);
+
+	SDL_RenderPresent(Renderer);
+
+	SDL_Delay(3000);// set window delay
+
+	return 0;
+}
