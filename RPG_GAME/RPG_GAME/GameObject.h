89aa76f3aa49c3292198bb46e099443bbc5ff5cb
GraphicsCodeBase <107340913+GraphicsCodeBase@users.noreply.github.com>
2024-04-30 17:59:00 +0800

    made a game object class

---
+++ RPG_GAME/RPG_GAME/GameObject.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Game.hpp"
+class GameObject
+{
+	public:
+		//creating the constructor.
+		GameObject(const char* textureSheet, SDL_Renderer* ren);
+		~GameObject();//destructor.
+
+		void Update();
+		void Render();
+
+	private:
+		int xPos;
+		int yPos;
+
+
+		SDL_Texture* objTexture;
+		SDL_Rect srcRect, destRect;
+		SDL_Renderer* renderer;
+};
